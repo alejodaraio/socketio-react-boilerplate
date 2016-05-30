04340e737383365f6c33b5f67b9764b6bb17b002
adaraio <alejo_daraio@artear.com>
2016-05-30 19:38:56 -0300

    ConnectorWrapper

--- app/client/index.jsx
+++ app/client/index.jsx
@@ -5,7 +5,7 @@ import io from 'socket.io-client'
 import ConnectorWrapper from './connectors/Socket/ConnectorWrapper'
 import Main from './containers/Main/Main.jsx'
 
-let connection = new ConnectorWrapper(io, 'http://localhost:3000');
+let connection = new ConnectorWrapper(io, 'http://localhost:3000').connect();
 
 ReactDOM.render(
     <Router history={browserHistory}>
